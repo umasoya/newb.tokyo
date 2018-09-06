23398aeba4b32b591bb36e28d4ba70ac4a974050
umasoya <basilisk.0331@icloud.com>
2018-09-06 14:55:53 +0900

    update eslint

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -25,6 +25,9 @@ module.exports = {
                 exclude: /node_modules/,
                 use: {
                     loader: 'eslint-loader'
+                },
+                options: {
+                    fix: true
                 }
             },
             {
