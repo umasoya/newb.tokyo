7223079f33f3d63a3fac7f679d7cba060ec4bb65
umasoya <basilisk.0331@icloud.com>
2018-12-07 12:09:10 +0900

    refactoring clipboard.js

--- assets/js/partial/clipboard.js
+++ assets/js/partial/clipboard.js
@@ -1,12 +1,14 @@
 import ClipboardJS from "clipboard";
 import tippy from "tippy.js";
 
+const MY_SWITCH_FIREND_CODE = 'SW-1510-1283-3250';
+
 // friend_cd copied to clipboard
 new ClipboardJS('#nintendo-switch');
 
 // show tooltip
 const friend_cd = document.createElement('div');
-friend_cd.innerHTML = 'Copied to clipboard<br />【SW-1510-1283-3250】';
+friend_cd.innerHTML = 'Copied to clipboard<br />【' + MY_SWITCH_FIREND_CODE + '】';
 tippy('#nintendo-switch', {
     trigger: 'click',
     html: friend_cd
