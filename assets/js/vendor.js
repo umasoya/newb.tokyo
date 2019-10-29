8c3a8e3638a71be90ee156dd33c5b2a8fbdfbe08
umasoya <basilisk.0331@icloud.com>
2019-10-29 03:05:09 +0000

    assets rebuild

--- assets/js/vendor.js
+++ assets/js/vendor.js
@@ -39,7 +39,5 @@ InstantClick.init();
 // }}}
 
 // {{1 fontawesome
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/all';
 // }}
