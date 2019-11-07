b7716f300f4ad3e425bf43c55efcabdf6df4ae9a
umasoya <basilisk.0331@icloud.com>
2019-11-07 08:32:32 +0000

    assoc asset files

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -13,7 +13,7 @@ module.exports = {
         extensions: ['.js'],
     },
     output: {
-        path: path.resolve(__dirname, "docs/js"),
+        path: path.resolve(__dirname, "static/js"),
         filename: 'app.js'
     },
     module: {
