f5b93bd2ce38dc1160240fbf8f060214ccf2ad24
umasoya <basilisk.0331@icloud.com>
2019-12-23 09:18:58 +0000

    init browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const webpack = require('webpack');
-const webpackStream = require('webpack-stream');
-const webpackConfig = require('./webpack.config.js');
+const webpack= require('webpack-stream');
+const browserSync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
 
 const paths = {
     'sass': { 'src': './assets/sass/*.scss', 'dest': './docs/css' },
@@ -10,14 +10,33 @@ const paths = {
 };
 
 gulp.task('webpack', () => {
-    return webpackStream(webpackConfig, webpack)
+    return gulp.src()
+        .pipe(webpack(require('./webpack.config.js')))
         .pipe(gulp.dest(paths.js.dest))
+        .pipe(browserSync.stream())
     ;
 });
 
 gulp.task('sass', () => {
     return gulp.src(paths.sass.src)
         .pipe(sass({ outputStyle: 'expanded' })).on('error', sass.logError)
+        .pipe(sourcemaps.write('./', {
+            includeContent: false,
+            sourceRoot: '../assets/sass',
+        }))
         .pipe(gulp.dest(paths.sass.dest))
+        .pipe(browserSync.stream())
     ;
 });
+
+gulp.task('bs', () => {
+    return browserSync.init(require('./bs-config.js'));
+});
+
+gulp.watch('watch', () => {
+    gulp.watch(paths.js.src, gulp.task('webpack'));
+    gulp.watch(paths.sass.src, gulp.task('sass'));
+});
+
+gulp.task('build', gulp.parallel('webpack', 'sass'));
+gulp.task('default', gulp.parallel('webpack', 'sass', 'bs'));
